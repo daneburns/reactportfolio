04b47c74e92930e416143b792bd487e22804d8d2
Dane Burns <dane@lizaburnsandassociates.com>
2020-04-10 11:04:00 -0700

    projects

--- portfolio/src/components/Modal.js
+++ portfolio/src/components/Modal.js
@@ -1,37 +1,68 @@
 import React from "react";
-import {useState} from "react";
-import {Modal} from "react-bootstrap"
-import {Button} from "react-bootstrap"
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import bujo from "./assets/gifs/bulletjournal.gif";
+import grocerEase from "./assets/gifs/grocerEase.gif";
+import weatherDash from "./assets/gifs/weatherDash.gif";
+import reactEmployee from "./assets/gifs/reactEmployee.gif"
+import sobered from "./assets/gifs/sobered.png"
+import fitness from "./assets/gifs/fitness.gif"
 
 function ModalExample(props) {
-    console.log(props)
-    const [show, setShow] = useState(false);
+  let imgPath;
+  switch (props.imgsource) {
+    case "bujo":
+      imgPath = bujo;
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+      break;
+    case "grocerEase":
+      imgPath = grocerEase;
+      break;
+    case "weatherDash":
+      imgPath = weatherDash;
+      break;
+    case "reactEmployee":
+      imgPath = reactEmployee;
+      break;
+      case "sobered":
+        imgPath = sobered;
+        break;
+        case "fitness":
+          imgPath = fitness;
+          break;
+
+    default:
+      break;
+  }
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
-    <Button variant="primary" onClick={handleShow}>
-      Launch demo modal
-    </Button>
+      <Button variant="light" onClick={handleShow} className='p-5'>
+        {props.title}
+      </Button>
 
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>{props.title}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleClose}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  </>
-
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img className="img-fluid" src={imgPath}></img>
+          <hr></hr>
+          {props.description}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -2,42 +2,37 @@ import React from "react";
 import ModalExample from "./Modal"
 
 function Project() {
+    const description = <p> Bullet Journal was a sweet project</p>
+    const grocerDesc = <p>GrocerEASE was awesome.</p>
+    const weatherDesc = <p>The weather dashboard was awesome.</p>
+    const empDesc = <p>The react employee app was awesome.</p>
+    const soberedDesc = <p>The sobered app was awesome.</p>
+    const fitnessTrack = <p>The fitness tracker was awesome.</p>
   return (
-    <div className="container bg-black text-white mt-5 mx-auto">
-      <div className="row text-center">
-        <div className="col-4 m-2">
-            <ModalExample title="Bullet Journal"/>
+    <div className="container bg-black text-white mt-5 mx-auto p-5">
+      <div className="row text-center m-5">
+        <div className="col-4">
+            <ModalExample title="Bullet Journal" imgsource="bujo" description={description}/>
         </div>
         <div className="col-4">
-          <p>Other Project</p>
+            <ModalExample title="Weather Dashboard" imgsource="weatherDash" description={weatherDesc}/>
         </div>
         <div className="col-4">
-          <p>Other Project</p>
+            <ModalExample title="grocerEase" imgsource="grocerEase" description={grocerDesc}/>
+        </div>
+        </div>
+        <div className='row text-center m-5'>
+        <div className="col-4">
+            <ModalExample title="React Employee Directory" imgsource="reactEmployee" description={empDesc}/>
+        </div>
+        <div className="col-4">
+            <ModalExample title="Sobered" imgsource="sobered" description={soberedDesc}/>
+        </div>
+        <div className="col-4">
+            <ModalExample title="Fitness Tracker" imgsource="fitness" description={fitnessTrack}/>
+        </div>
         </div>
       </div>
-      <div className="row text-center">
-        <div className="col-4">
-          <p>Other Project</p>
-        </div>
-        <div className="col-4">
-          <p>Other Project</p>
-        </div>
-        <div className="col-4">
-          <p>Other Project</p>
-        </div>
-      </div>
-      <div className="row text-center">
-        <div className="col-4">
-          <p>Other Project</p>
-        </div>
-        <div className="col-4">
-          <p>Other Project</p>
-        </div>
-        <div className="col-4">
-          <p>Other Project</p>
-        </div>
-      </div>
-    </div>
   );
 }
 
